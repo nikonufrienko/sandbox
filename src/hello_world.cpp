d6c0e27fd4afa3fee72a3231465242ae3224bcd3
howl <howl69@yandex.ru>
2021-07-30 17:48:33 +0300

    hello world!

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace std;
-
 int main()
 {
-    cout << "Hello, world!" << endl;
+    int fd;
+    char * myfifo = "myfifo";
+    mkfifo(myfifo, 0777);
+    fd = open("myfifo", O_WRONLY);
+    dup2(fd, STDOUT_FILENO);
+    printf("hello world");
+    fflush(stdout);
+    close(fd);
     return 0;
 }
