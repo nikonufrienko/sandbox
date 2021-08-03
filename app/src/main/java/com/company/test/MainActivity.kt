54bff2f4035ce72ce1ebea1f43d46a6344d49cc7
Kazakov Mihail <advancerman@gmail.com>
2021-08-03 15:33:13 +0300

    Filled addresses text view with data

--- app/src/main/java/com/company/test/MainActivity.kt
+++ app/src/main/java/com/company/test/MainActivity.kt
@@ -3,6 +3,8 @@ package com.company.test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.company.test.databinding.ActivityMainBinding
+import java.net.Inet4Address
+import java.net.NetworkInterface
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -13,6 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.button2.setOnClickListener { refreshAddresses() }
+        refreshAddresses()
         IperfRunner(applicationContext.filesDir.absolutePath, this)
+    }
+
+    private fun refreshAddresses() {
+        binding.sampleText.text = NetworkInterface.getNetworkInterfaces()
+            .toList()
+            .filter { it.inetAddresses.hasMoreElements() }
+            .joinToString(separator = System.lineSeparator()) { networkInterface ->
+                val addresses = networkInterface.inetAddresses.toList()
+                    .filterIsInstance<Inet4Address>()
+                    .joinToString(separator = ", ")
+                "${networkInterface.displayName}: $addresses"
+            }
     }
 }
