3be6a2fc9e71391cebfe4d9edb82d7f4886648e1
nikonufrienko <nikonufrienko@gmail.com>
2021-08-06 11:54:13 +0300

    initial commit

---
+++ app/src/main/java/com/company/test/pingChecker.kt
@@ -0,0 +1,48 @@
+package com.company.test
+
+import android.util.Log
+import kotlinx.coroutines.delay
+import java.io.IOException
+import java.net.Inet4Address
+import java.net.InetAddress
+
+suspend fun doPingTest(
+    currValueSetter: (String) -> Unit,
+    ip: String,
+    port: Int = 7,
+    time: Int = 3000,
+    delayValue: Long = 100
+) {
+    try {
+        val start = System.currentTimeMillis()
+        val pingChecker = PingCheckClient()
+        var number = 0
+        var counter = 0
+        var mistakesCounter = 0
+        while (System.currentTimeMillis() - start < time) {
+            number++
+            val currValue = pingChecker.doPing(InetAddress.getByName(ip), 7, 100)
+            if (currValue == -1)
+                mistakesCounter++
+            else {
+                currValueSetter(currValue.toString())
+                counter += currValue
+            }
+            delay(delayValue)
+        }
+
+        if (mistakesCounter == number)
+            currValueSetter("Error")
+        else {
+            val result =
+                ((counter.toDouble()) / (number.toDouble() - mistakesCounter.toDouble())).toString()
+            if (result.length > 5)
+                currValueSetter(result.substring (0, 5))
+            else
+                currValueSetter(result)
+        }
+    } catch (e: Exception) {
+        currValueSetter("Error")
+    }
+}
+
