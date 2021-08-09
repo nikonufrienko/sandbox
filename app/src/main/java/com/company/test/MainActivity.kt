33a01a72310c508fd65aa3d21fd22a1eb7394147
nikonufrienko <nikonufrienko@gmail.com>
2021-08-09 15:02:14 +0300

    initial commit

--- app/src/main/java/com/company/test/MainActivity.kt
+++ app/src/main/java/com/company/test/MainActivity.kt
@@ -2,16 +2,25 @@ package com.company.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.widget.ArrayAdapter
 import com.company.test.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 import java.net.Inet4Address
 import java.net.NetworkInterface
+import java.util.concurrent.ConcurrentLinkedDeque
+import java.util.concurrent.atomic.AtomicBoolean
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var iperfRunner: IperfRunner
-    var isPingInChecking = false
+
+    private val justStopICMPPingPingFlag = AtomicBoolean(false)
+    private val justICMPPingInChecking = AtomicBoolean(false)
+    private var isPingInChecking = AtomicBoolean(false)
+    private var stopPingFlag = AtomicBoolean(false)
+    private var isPingICMPInChecking = AtomicBoolean(false)
 
     @Volatile
     var pingValueBuffer = "---"
@@ -41,27 +50,33 @@ class MainActivity : AppCompatActivity() {
         binding.pingTestButt.setOnClickListener {
             pingTestButtonAction()
         }
+        binding.checkIcmpPingButt.setOnClickListener {
+            startICMPPing()
+        }
+        binding.iperfOutput.movementMethod = ScrollingMovementMethod()
+
+        binding.justPingButt.setOnClickListener {
+            justICMPPing()
+        }
     }
 
     private fun pingTestButtonAction() = runBlocking {
-        if (!isPingInChecking) {
-            isPingInChecking = true
-            binding.pingTestButt.text = "TESTING..."
+        if (!isPingInChecking.get()) {
+            isPingInChecking.set(true)
+            binding.pingTestButt.text = getString(R.string.pingTesting)
             CoroutineScope(Dispatchers.IO).launch {
                 doPingTest(
                     { value: String -> pingValueBuffer = value },
                     binding.serverIP.text.toString()
                 )
-                isPingInChecking = false
+                isPingInChecking.set(false)
             }
             CoroutineScope(Dispatchers.Main).launch {
                 do {
                     delay(1)
                     binding.pingValue.text = pingValueBuffer
-                } while (isPingInChecking)
-                if (binding.pingValue.text != pingValueBuffer)
-                    binding.pingValue.text = pingValueBuffer
-                binding.pingTestButt.text = "TEST PING"
+                } while (isPingInChecking.get()|| binding.pingValue.text != pingValueBuffer)
+                binding.pingTestButt.text = getString(R.string.testPing)
             }
         }
     }
@@ -87,7 +102,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun startIperf() {
         iperfRunner.start(binding.iperfArgs.text.toString())
-
         binding.iperfArgs.isEnabled = false
         binding.startStopButton.text = applicationContext.getString(R.string.stopIperf)
         binding.startStopButton.setOnClickListener { stopIperf() }
@@ -95,9 +109,70 @@ class MainActivity : AppCompatActivity() {
 
     private fun stopIperf() {
         iperfRunner.stop()
-
         binding.iperfArgs.isEnabled = true
         binding.startStopButton.text = applicationContext.getString(R.string.startIperf)
         binding.startStopButton.setOnClickListener { startIperf() }
+    }
+
+    private fun startICMPPing() = runBlocking {
+        if (!isPingICMPInChecking.get()) {
+            val pinger = ICMPPing()
+            Log.d("", "ICMP ping has been ran")
+            isPingICMPInChecking.set(true)
+            val host = binding.serverIP.text.toString()
+            val stringDeque: ConcurrentLinkedDeque<String> = ConcurrentLinkedDeque()
+
+            CoroutineScope(Dispatchers.IO).launch {
+                pinger.performPingWithArgs(host, stringDeque)
+                isPingICMPInChecking.set(false)
+                Log.d("", "DONE!")
+            }
+
+            CoroutineScope(Dispatchers.Main).launch {
+                binding.checkIcmpPingButt.text = getString(R.string.bigStop)
+                while ((isPingICMPInChecking.get() || stringDeque.isNotEmpty())) {
+                    delay(10)
+                    if (stringDeque.isNotEmpty())
+                        binding.iperfOutput.append(stringDeque.removeFirst() + "\n")
+                    if(stopPingFlag.get()){
+                        pinger.stopExecuting()
+                        stopPingFlag.set(false)
+                    }
+                }
+                binding.checkIcmpPingButt.text = getString(R.string.performAsCommand)
+            }
+        } else {
+            Log.d("PERFORM AS COMMAND", "STOP")
+            stopPingFlag.set(true)
+        }
+    }
+
+
+    private fun justICMPPing() = runBlocking {
+        if (!justICMPPingInChecking.get()) {
+            justICMPPingInChecking.set(true)
+            binding.justPingButt.text = getString(R.string.bigStop)
+            val pinger = ICMPPing()
+            CoroutineScope(Dispatchers.IO).launch {
+                pinger.justPingByHost(
+                    binding.serverIP.text.toString()
+                ) { value: String -> pingValueBuffer = value }
+                Log.d("", "DONE!")
+                justICMPPingInChecking.set(false)
+            }
+            CoroutineScope(Dispatchers.Main).launch {
+                do {
+                    delay(10)
+                    binding.pingValue.text = pingValueBuffer
+                    if (justStopICMPPingPingFlag.get()){
+                        pinger.stopExecuting()
+                        justStopICMPPingPingFlag.set(false)
+                    }
+                } while (justICMPPingInChecking.get() || binding.pingValue.text != pingValueBuffer)
+                binding.justPingButt.text = getString(R.string.justPing)
+            }
+        } else {
+            justStopICMPPingPingFlag.set(true)
+        }
     }
 }
