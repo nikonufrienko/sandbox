f0bcfe644fc1d1b666d540bf7ac5f8f446403231
nikonufrienko <nikonufrienko@gmail.com>
2021-08-09 16:01:54 +0300

    added icmp ping

--- app/src/main/java/com/company/test/ICMPPing.kt
+++ app/src/main/java/com/company/test/ICMPPing.kt
@@ -1,16 +1,13 @@
 package com.company.test
 
-import android.util.Log
 import kotlinx.coroutines.delay
 import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.IOException
 import java.io.InputStreamReader
 import java.util.concurrent.ConcurrentLinkedDeque
-import java.util.concurrent.atomic.AtomicBoolean
 
 class ICMPPing {
-    //private val stopFlag: AtomicBoolean = AtomicBoolean(false)
     private val currentProcess: Process = ProcessBuilder("sh").redirectErrorStream(true).start()
 
     suspend fun performPingWithArgs(args: String, linesDeque: ConcurrentLinkedDeque<String>) {
@@ -21,12 +18,10 @@ class ICMPPing {
         var line: String?
         try {
             while (reader.readLine().also { line = it } != null) {
-                line.let { Log.d("", it.toString()) }
                 linesDeque.addLast(line)
                 delay(10)
             }
         } catch (e: IOException) {
-            Log.d("", "was interrupted")
         }
     }
 
@@ -38,19 +33,16 @@ class ICMPPing {
         var line: String?
         try {
             while (reader.readLine().also { line = it } != null) {
-                line.let { Log.d("", it.toString()) }
                 line!!.split(" ").forEach {
                     if (it.contains("time="))
                         currValueGetter(it.split("=")[1])
                 }
             }
         } catch (e: IOException) {
-            Log.d("", "was interrupted")
         }
     }
 
     fun stopExecuting() {
-        Log.d("", "STOP EXECUTING")
         currentProcess.destroy()
     }
 }
